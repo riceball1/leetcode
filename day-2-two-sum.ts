f15cde34ff11bae7052665e5f090258af3795d93
Dana Ng <ngdana1@gmail.com>
2021-07-03 23:51:39 -0700

    Add comments

--- day-2-two-sum.ts
+++ day-2-two-sum.ts
@@ -14,7 +14,15 @@
 
 // attempt one - doesn't pass all tests
 function twoSum(nums: number[], target: number): number[] {
+    // initialize the return value as an empty array
     let indices = [];
+
+
+    /**
+     * There is a difference for when the nums length == 2, this means
+     * that we already know there can only be two possible indicies
+     * therefore it be easy just to compare these two values
+     */
     if (nums.length === 2) {
         const sum = nums[0] + nums[1];
         if (sum === target) {
@@ -23,6 +31,11 @@ function twoSum(nums: number[], target: number): number[] {
         return indices;
     }
     
+    /**
+     * I used two for-loops thinking they will help me compare each element 
+     * I was able to find the sum of each adjacent element and then compare that
+     * to the target value, and find the indicies this way
+     */
     for(let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
             let sum = nums[i] + nums[j + 1] 
@@ -42,7 +55,7 @@ function twoSumAttemptTwo(nums: number[], target: number): number[] {
     // use a hash map
     let numsIndicies = {}
 
-    
+
     
     return [];
 }
