6bfe3bef7df82ed2a67e1a778fd17d4a7f9b88a2
Dana Ng <ngdana1@gmail.com>
2021-07-03 23:44:51 -0700

    Add day 2 leetcode

---
+++ day-2-two-sum.ts
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/two-sum/
+
+/**
+ * 
+ * Given an array of integers nums and an integer target, 
+ * return indices of the two numbers such that they add up 
+ * to target. You may assume that each input would have 
+ * exactly one solution, and you may not use the same element 
+ * twice.
+ * 
+ * You can return the answer in any order.
+ */
+
+
+function twoSum(nums: number[], target: number): number[] {
+    let indices = [];
+    if (nums.length === 2) {
+        const sum = nums[0] + nums[1];
+        if (sum === target) {
+            indices = [0, 1]
+        }
+        return indices;
+    }
+    
+    for(let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            let sum = nums[i] + nums[j + 1] 
+            if (sum === target) {
+                indices = [i, j + 1];
+            }
+        }
+    }
+    
+    return indices;
+
+};
