f37056275f176498956ac5f5d7f2e89eef5be7ba
Dana Ng <ngdana1@gmail.com>
2021-07-04 21:26:58 -0700

    Add day 3

---
+++ day-3-valid-palindrome.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+/**
+ * Given a string s, determine if it is a palindrome,
+ * considering only alphanumeric characters and ignoring cases.
+ */
+
+function isPalindrome(s: string): boolean {
+  let isValidPalindrome = false;
+
+  let modifiedString = s.trim();
+
+  return isValidPalindrome;
+}
