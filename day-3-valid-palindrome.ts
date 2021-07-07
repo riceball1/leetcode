4910b3122307621a7da6a699d478f76ecef4a98e
Dana Ng <ngdana1@gmail.com>
2021-07-06 18:30:11 -0700

    Update day 3 code

--- day-3-valid-palindrome.ts
+++ day-3-valid-palindrome.ts
@@ -5,10 +5,16 @@
  * considering only alphanumeric characters and ignoring cases.
  */
 
-function isPalindrome(s: string): boolean {
-  let isValidPalindrome = false;
-
-  let modifiedString = s.trim();
-
-  return isValidPalindrome;
-}
+ function isPalindrome(s: string): boolean {    
+    // remove white spaces and non alphanumeric chars
+    // lowercase the modified string
+    let modifiedString = s.replace(/[\W_]+/g,"").toLowerCase();
+    // split a string to reverse it, then join it back
+    // due to the use of splitting and reversing here and then joining back
+    // this may be a O(n) situation or O(n^2) given that there's a loop 
+    // and nested loop involved
+    let reversedString = modifiedString.split('').reverse().join('');
+    
+    // check if the strings are the same
+    return modifiedString === reversedString;
+};
