7cd174fe8b89e7fbff16cd31a854d2c285f882ee
Dana Ng <ngdana1@gmail.com>
2021-07-09 23:07:56 -0700

    Update day 5

--- day-5-climbing-stairs.ts
+++ day-5-climbing-stairs.ts
@@ -24,3 +24,16 @@
     
     return dp[n];   
 };
+
+// another example with faster time
+function climbStairsTwo(n: number): number {
+    const dp = [1,1]
+    
+    for (let i = 2; i <= n; i++) {
+        const result = dp[0] + dp[1];
+        dp[0] = dp[1];
+        dp[1] = result
+    }
+    
+    return dp[1];   
+};
