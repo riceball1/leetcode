7951d3a879a2bec037c471db2921ff4d15a8164b
Dana Ng <ngdana1@gmail.com>
2021-07-12 20:33:54 -0700

    Add day 10

---
+++ day-10-longest-substring-no-repeating-char.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+/**
+ * Given a string s, find the length of the 
+ * longest substring without repeating characters.
+ */
+
+ function lengthOfLongestSubstring(s: string): number {
+
+};
