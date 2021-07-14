8b89f91c0b4aeaa7c11236bc2350164d9cf13b85
Dana Ng <ngdana1@gmail.com>
2021-07-13 22:14:24 -0700

    Update day 10

--- 30-days-v1/day-10-longest-substring-no-repeating-char.ts
+++ 30-days-v1/day-10-longest-substring-no-repeating-char.ts
@@ -6,8 +6,30 @@
  */
 
  function lengthOfLongestSubstring(s: string): number {
-
-};
+    const seen = {};
+    let longest = 0; // 0
+    let currentLongest = 0;  // 3
+    let stringLength = s.length;
+    let index = 0;
+    let startIndex = 0;
+    
+    while(index < stringLength) {
+        if(seen[s[index]]) {
+        longest = currentLongest >= longest ? currentLongest : longest;
+        currentLongest = currentLongest - seen[s[index]]  + startIndex;                 
+        startIndex = seen[s[index]] + 1;
+        seen[s[index]] = index;
+          index += 1;
+      } else {
+        currentLongest += 1;
+        seen[s[index]] = index;
+        index += 1;
+      }
+    }
+      
+      return longest;
+  
+  };
 
 
 
@@ -17,5 +39,5 @@
  * - would want to keep track of the previous chars to make sure there aren't any repeating
  * - thinking about how we can increase the window (sliding) size of the substring each time 
  * there's a unique char, and if not then remember that part and then start another substring
- * - compare the substrings and return the longest length one
+ * - compare the substrings and return the length of longest one
  */
