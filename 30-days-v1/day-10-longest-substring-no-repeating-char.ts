18cbbdeed6be81a62e091341f2aa4aaa97616557
Dana Ng <ngdana1@gmail.com>
2021-07-13 20:57:42 -0700

    Add notes for day 10

--- 30-days-v1/day-10-longest-substring-no-repeating-char.ts
+++ 30-days-v1/day-10-longest-substring-no-repeating-char.ts
@@ -8,3 +8,14 @@
  function lengthOfLongestSubstring(s: string): number {
 
 };
+
+
+
+/**
+ * Initial thoughts to approaching this problem:
+ * 
+ * - would want to keep track of the previous chars to make sure there aren't any repeating
+ * - thinking about how we can increase the window (sliding) size of the substring each time 
+ * there's a unique char, and if not then remember that part and then start another substring
+ * - compare the substrings and return the longest length one
+ */
