02b35531c19e4491ce812e8599e93ae06eb7dc04
Dana Ng <ngdana1@gmail.com>
2021-07-16 19:40:49 -0700

    Add day 13

--- 30-days-v1/day-11-build-array-permutations.ts
+++ 30-days-v1/day-11-build-array-permutations.ts
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/build-array-from-permutation/
 
+
 /**
  * Given a zero-based permutation nums (0-indexed), 
  * build an array ans of the same length where ans[i] = nums[nums[i]] 
@@ -9,6 +10,39 @@
  * integers from 0 to nums.length - 1 (inclusive).
  */
 
+/**
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] < nums.length
+ * The elements in nums are distinct.
+ */
+
  function buildArray(nums: number[]): number[] {
 
 };
+
+
+
+
+/**
+ * Thoughts?
+ * 
+ * First I'm a bit confused by what's it asking
+ * What is a permutation?
+ * 
+ * def: technique that determines the number of 
+ * possible arrangements in a set when the 
+ * order of the arrangements matters
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [0,2,1,5,3,4]
+ * Output: [0,1,2,4,5,3]
+ * 
+ * Example 2:
+ * Input: nums = [5,0,1,2,3,4]
+ * Output: [4,5,0,1,2,3]
+ * 
+ * 
+ */
