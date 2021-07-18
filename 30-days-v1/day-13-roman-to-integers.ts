50044880b0c8d72d45fdad7b5f2d9e3e944c2579
Dana Ng <ngdana1@gmail.com>
2021-07-17 22:05:53 -0700

    Update day 13

---
+++ 30-days-v1/day-13-roman-to-integers.ts
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+/**
+ * Given a roman numeral, convert it to an integer.
+ */
+
+
+function romanToInt(s: string): number {
+    // roman values
+    const romanValues = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000,
+    };
+    
+    let total = 0;
+    
+    // O(n)
+    for(let i = 0; i < s.length; i++) {
+
+        /**
+        Need to add conditonals to handle 
+        IV, IX = 4 and 9
+        XL XC = 40 and 90
+        CD CM = 400 and 900
+        */
+        
+        total += romanValues[s[i]];
+    }
+    
+    return total;
+    
+
+};
