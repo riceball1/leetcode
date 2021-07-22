dda8406d526d6dba594d4df6d02fd37f8ac76049
Dana Ng <ngdana1@gmail.com>
2021-07-22 14:25:44 -0700

    Add day 15

---
+++ 30-days-v1/day-15-merge-two-sorted-lists.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Merge two sorted linked lists and return it as a sorted list. The list 
+ * should be made by splicing together the nodes of the first two lists.
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+
+};
--- README.md
+++ README.md
@@ -26,7 +26,7 @@ H = Hard
 | Day 12 - 7/12/2021 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/)| ❌| E |
 | Day 13 - 7/16/21 | [Roman to Integers](https://leetcode.com/problems/roman-to-integer/)|  ❌| E|
 | Day 14 - 7/19/21 | [Validate Binary Search Tree](https://leetcode.com/problems/validate-binary-search-tree/)| |M |
-| Day 15 | TBD| | |
+| Day 15 - 7/22/21 | [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/)| | E|
 | Day 16 | TBD| | |
 | Day 17 | TBD| | |
 | Day 18 | TBD| | |
