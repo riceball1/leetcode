58ff5251d4e06adb629315ebe4a046dd410e8bd3
Dana Ng <ngdana1@gmail.com>
2021-07-31 14:04:41 -0700

    Update linked list

--- 30-days-v1/day-17-linked-list-cycle.ts
+++ 30-days-v1/day-17-linked-list-cycle.ts
@@ -13,5 +13,16 @@
  */
 
  function hasCycle(head: ListNode | null): boolean {
-    
+    // handle linkedList without next val
+    if (head === null) return false;
+    let count = 0;
+    function checkHasCycle(node) {
+        console.log(node.next.val)
+        count += 1;   
+        if (node?.next || count === 3) return false;
+         
+        return checkHasCycle(node?.next) 
+       
+    }
+    return checkHasCycle(head);
 };
