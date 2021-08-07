e1c69c30fa6e458c10a735b8e756fbebac5e4088
Dana Ng <ngdana1@gmail.com>
2021-08-06 22:16:27 -0700

    Add day 20 sort linked list

---
+++ 30-days-v1/day-20-sort-linked-list.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/sort-list/
+
+/**
+ * Given the head of a linked list, return 
+ * the list after sorting it in ascending order.
+ * 
+ * Follow up: Can you sort the linked list 
+ * in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function sortList(head: ListNode | null): ListNode | null {
+
+};
--- README.md
+++ README.md
@@ -33,7 +33,7 @@ H = Hard
 | Day 17 - 7/29/21 | [Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/)|❌ |E |
 | Day 18 - 8/5/21 | [Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/)| |M |
 | Day 19 - 8/6/21 | [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/)|❌  | E|
-| Day 20 | TBD| | |
+| Day 20 - 8/6/21 | [Sort Linked List](https://leetcode.com/problems/sort-list/)| |M |
 | Day 21 | TBD| | |
 | Day 22 | TBD| | |
 | Day 23 | TBD| | |
