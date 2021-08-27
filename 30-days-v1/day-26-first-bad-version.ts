a5ebebe5429b59c3b366bad3b5a200825c3b8aa3
Dana Ng <ngdana1@gmail.com>
2021-08-26 19:39:20 -0700

    Update day 26

--- 30-days-v1/day-26-first-bad-version.ts
+++ 30-days-v1/day-26-first-bad-version.ts
@@ -14,12 +14,22 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
- var solution = function(isBadVersion) {
+ const solution = function(isBadVersion) {
     /**
      * @param {integer} n Total versions
      * @return {integer} The first bad version
      */
     return function(n) {
-        
+        // binary search
+       let start = 1, end = n;
+       while(start < end){
+           let mid = Math.floor(start + (end-start) / 2);
+           if(isBadVersion(mid)){
+                end = mid; // look on left side of mid
+           }else{
+               start = mid+1; // look on the right side of mid
+           }
+       }
+       return start;
     };
 };
