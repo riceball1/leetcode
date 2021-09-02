f8a2c05ea50e1416545edb510bb4c2a2f6e70c76
Dana Ng <ngdana1@gmail.com>
2021-09-01 23:46:54 -0700

    Update day 27

--- 30-days-v1/day-27-squares-of-sorted-array.ts
+++ 30-days-v1/day-27-squares-of-sorted-array.ts
@@ -15,3 +15,30 @@
     return sortedSquaredNumsArray.sort((a, b) => a - b);
 };
 
+
+// The only thing I didn't understand for this
+// was that I needed to know that the 2nd param in the for-loop was an exit condition
+// so I needed to get 1>=0 working in order to get the squaredNums[0] assigned
+
+function sortedSquaresTwoPointer(nums: number[]): number[] {
+    // two-pointer approach
+    let squaredNums = [];
+    let pointerOne = 0;
+    let pointerTwo = nums.length - 1;
+    
+    for(let i = nums.length - 1; i >= 0; i--) {
+        let a = nums[pointerOne] * nums[pointerOne];
+        let b = nums[pointerTwo] * nums[pointerTwo];
+        if (a > b) {
+             pointerOne++;
+            squaredNums[i] = a;
+        } else {
+            pointerTwo--;
+            squaredNums[i] = b;
+        } 
+       
+    }
+    
+    return squaredNums;
+    
+};
