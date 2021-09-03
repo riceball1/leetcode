c70b71c3f9a0150ff2ca7c3e81affa86708b4a5e
Dana Ng <ngdana1@gmail.com>
2021-09-02 19:09:08 -0700

    Add day 28

---
+++ 30-days-v1/day-28-rotate-array.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/rotate-array/
+
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+ function rotate(nums: number[], k: number): void {
+
+};
