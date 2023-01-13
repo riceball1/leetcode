c098af5be73ce419abcab2dd161b03cbdec65a92
Dana Ng <ngdana1@gmail.com>
2023-01-12 19:22:35 -0800

    Add insertion sort algo

--- Random-Algos/sorting-algos.js
+++ Random-Algos/sorting-algos.js
@@ -2,7 +2,7 @@
 
 The following are common sorting algorithims:
 - Bubble Sort: Time: O(n^2)
-- Insertion Sort; Time: O(n^2)
+- Selection Sort; Time: O(n^2)
 - Merge Sort; Time O(nlogn)
 
 */
@@ -35,18 +35,77 @@ function bubbleSort(nums) {
 }
 
 // Test Cases
-console.log(bubbleSort([4, 3, 2]), [2, 3, 4])
-console.log(bubbleSort([-4, 3, 2]), [-4, 2, 3])
+console.log('bubbleSort', bubbleSort([4, 3, 2]), [2, 3, 4])
+console.log('bubbleSort', bubbleSort([-4, 3, 2]), [-4, 2, 3])
 
 
 
 /*
 
-Insertion Sort
+Selection Sort
+Time Complexity: O(n^2)
+Space Complexity: O(1) - inplace sorting, no extra space is used
+
+Unstable; selection sort only works efficiently when the small set of elements is involved or the list is partially previously sorted
 
 */
 
+function selectionSort(nums) {
 
+    for(let i = 0; i < nums.length; i++) {
+        // find min value in unsorted region
+        let minIndex = i;
+        for(let j = i + 1; j < nums.length; j++) {
+            // update the minIndex if the value at j is lower than the current min value
+            if(nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // after finding the min value in the unsorted region
+        // swap with the first unsorted value
+        nums[i], nums[minIndex] = nums[minIndex], nums[i]
+
+    }
+
+    return nums;
+
+}
+
+// Test Cases
+console.log('selectionSort', selectionSort([3, 2, 1, 5, 4]), [1, 2, 3, 4, 5])
+
+/*
+
+Insertion Sort
+Time Complexity: O(n^2)
+Space Complexity: O(1) - inplace sorting, no extra space is used
+
+Stable; Selection sort perform sorting by searching for the minimum value number and placing it into the first or last position according to the order (ascending or descending)
+
+*/
+
+function insertionSort(nums) {
+    
+    for(let i = 1; i < nums.length; i++) {
+        let key = nums[i] // set the key to current num
+        let j = i - 1;
+        while(j >= 0 && nums[j] > key) {
+            // keep swapping j up each time
+            // when the curr value is greater than key
+            nums[j + 1] = nums[j] 
+            j = j - 1;
+        }
+        // set next value to be the key
+        nums[j + 1] = key;
+    }
+
+
+    return nums;
+
+}
+
+// Test Cases
+console.log('insertionSort', insertionSort([3, 2, 1, 5, 4]), [1, 2, 3, 4, 5])
 
 /*
 
