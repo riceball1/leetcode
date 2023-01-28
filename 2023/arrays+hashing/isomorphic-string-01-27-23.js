0e1d4ce6c14d4f8fc241a474c2cd91935dcc7d63
Dana Ng <ngdana1@gmail.com>
2023-01-27 23:50:18 -0800

    Add update to isomorphic algo

--- 2023/arrays+hashing/isomorphic-string-01-27-23.js
+++ 2023/arrays+hashing/isomorphic-string-01-27-23.js
@@ -40,3 +40,19 @@ var isIsomorphic = function(s, t) {
     return true
     
 };
+
+
+// A cleaner solution I found:
+
+var isIsomorphic = function(s, t) {
+    var obj = {}; // used to check already seen values
+
+    for(var i = 0; i < s.length; i++){
+        if(!obj['s' + s[i]]) obj['s' + s[i]] = t[i];
+        if(!obj['t' + t[i]]) obj['t' + t[i]] = s[i];
+        if(t[i] != obj['s' + s[i]] || s[i] != obj['t' + t[i]]) {
+            return false;
+        } 
+    }
+    return true;
+};
