f1d77d1cf3f7996d08b34e19360f2e295e6c3dfb
Dana Ng <ngdana1@gmail.com>
2023-03-05 20:36:38 -0800

    Update algo w/ explanation

--- Leetcode-Data-Structure-1_2023/02-Day.ts
+++ Leetcode-Data-Structure-1_2023/02-Day.ts
@@ -24,6 +24,28 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     }
   }
   
+/*
+
+This is an explanation of the algorithm above used to merge two sorted arrays nums1 and nums2 into one sorted array nums1.
+
+Here's how the algorithm works:
+
+Initialize two pointers, index1 and index2, to the last elements of the original arrays, nums1 and nums2, respectively.
+
+We will start filling nums1 from the end, so set up a for loop starting from n + m - 1, where n and m are the sizes of nums2 and nums1 respectively, and iterate until i becomes less than 0 or index2 becomes less than 0.
+
+Inside the loop, compare the elements at nums1[index1] and nums2[index2] and place the larger element at nums1[i].
+
+If the current element from nums1 is greater than the current element from nums2, we place the current element from nums1 at nums1[i], decrement index1 and move on to the next element in nums1.
+
+If the current element from nums2 is greater than or equal to the current element from nums1, we place the current element from nums2 at nums1[i], decrement index2 and move on to the next element in nums2.
+
+Repeat this process until all elements from both arrays are merged into nums1.
+
+The idea behind the algorithm is to avoid creating a new array to merge the two arrays, as it would require O(n) space. Instead, we can take advantage of the fact that nums1 has extra space at the end and merge the two arrays from the end. This way, we avoid the need for extra space and perform the merge operation in place in O(n) time complexity.
+*/
+
+
 
 // https://leetcode.com/problems/two-sum/description
 
