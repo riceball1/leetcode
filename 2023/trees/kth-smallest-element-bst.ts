d0f5204c83f174c216d59e3bb5a0eb6a884a3056
Dana Ng <ngdana1@gmail.com>
2023-03-08 23:31:15 -0800

    update algo

--- 2023/trees/kth-smallest-element-bst.ts
+++ 2023/trees/kth-smallest-element-bst.ts
@@ -64,3 +64,86 @@ let tree = new TreeNode(
 const test = kthSmallest(tree, 2); // 3
 
 console.log(test);
+
+/*
+
+
+Follow up prompt:
+
+If the BST is modified often and we need to find the kth smallest value frequently, we can optimize by maintaining additional information in each node. Specifically, we can store the number of nodes in the subtree rooted at each node. Then, when we want to find the kth smallest value, we can use this information to quickly determine which subtree to search next. If k is less than the size of the left subtree of the root, we search for the kth smallest value in the left subtree. If k is greater than the size of the left subtree plus one, we search for the (k - size of left subtree - 1)th smallest value in the right subtree. If k is equal to the size of the left subtree plus one, we return the value of the root. This approach takes O(log n) time to find the kth smallest value, where n is the number of nodes in the BST.
+
+
+
+*/
+
+class TreeNodeAlternative {
+  val: number;
+  left: null | TreeNodeAlternative;
+  right: null | TreeNodeAlternative;
+  leftCount: number;
+
+  constructor(
+    val: number,
+    left: null | TreeNodeAlternative = null,
+    right: null | TreeNodeAlternative = null
+  ) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+    this.leftCount = 0; // number of nodes in left subtree
+  }
+}
+
+function insert(root, val) {
+  if (!root) {
+    return new TreeNodeAlternative(val);
+  }
+  if (val < root.val) {
+    root.left = insert(root.left, val);
+    root.leftCount++; // increment left subtree count
+  } else {
+    root.right = insert(root.right, val);
+  }
+  return root;
+}
+
+function deleteNode(root, val) {
+  if (!root) {
+    return null;
+  }
+  if (val < root.val) {
+    root.left = deleteNode(root.left, val);
+    root.leftCount--;
+  } else if (val > root.val) {
+    root.right = deleteNode(root.right, val);
+  } else {
+    if (!root.left) {
+      return root.right;
+    } else if (!root.right) {
+      return root.left;
+    } else {
+      let node = root.right;
+      while (node.left) {
+        node = node.left;
+      }
+      root.val = node.val;
+      root.right = deleteNode(root.right, node.val);
+    }
+  }
+  return root;
+}
+
+function kthSmallestAlternative(root, k) {
+  let count = root.leftCount + 1;
+  if (k === count) {
+    return root.val;
+  } else if (k < count) {
+    return kthSmallest(root.left, k);
+  } else {
+    return kthSmallest(root.right, k - count);
+  }
+}
+
+/*
+  With this optimized solution, we can find the kth smallest value in O(log n) time on average, where n is the number of nodes in the tree. The insert and delete operations also take O(log n) time on average, assuming the tree is balanced, so we can efficiently modify the tree as needed.
+  */
