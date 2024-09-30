2ed43623d6e4d38932ddf86e5346aae812474913
Dana Ng <ngdana1@gmail.com>
2024-09-30 00:00:08 -0700

    add algo for python

--- 2024/leetcode75/09_29_2024_Merge_Strings_Alternately.py
+++ 2024/leetcode75/09_29_2024_Merge_Strings_Alternately.py
@@ -1,1 +1,13 @@
-# TODO: write up algo for python
+def mergeAlternately(word1, word2):
+    word1Len = len(word1)
+    word2Len = len(word2)
+
+    newWord = ""
+
+    for i in range(max(word1Len, word2Len)):
+        if i < word1Len:
+            newWord += word1[i]
+        if i < word2Len:
+            newWord += word2[i]
+    
+    return newWord
